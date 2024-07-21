b3586d39d510091c9eb4877f8ab6065045474629
Vlad D <83023299+deesync@users.noreply.github.com>
2024-07-22 00:00:37 +0300

    Update db.php

--- db.php
+++ db.php
@@ -2,10 +2,10 @@
 session_start();
 
 $conn = mysqli_connect(
-  'localhost',
-  'root',
-  'password123',
-  'php_mysql_crud'
+  $host = getenv('DB_HOST'),
+  $username = getenv('DB_USER'),
+  $password = getenv('DB_PASSWORD'),
+  $dbname = getenv('DB_NAME')
 ) or die(mysqli_erro($mysqli));
 
 ?>
